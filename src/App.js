037885f4234be11d0ffdf2b74093daad39241975
Direwolf121 <jefflin1201@gmail.com>
2022-12-30 17:31:45 +0900

    emoji test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import './App.css';
 
+const displayEmojiName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: '😀',
+    name: "test grinning face"
+  },
+  {
+    emoji: '🎉',
+    name: "party popper"
+  },
+  {
+    emoji: '💃',
+    name: "woman dancing"
+  },
+  {
+    emoji: '😄',
+    name: "笑顔"
+  }
+];
+
 function App() {
-  return <h1>Hello, World</h1>;
+  const greeting = "greeting";
+  return(
+    <div className="container">
+      <h1 id={greeting}>Hello, World</h1>
+      <p>I am writing JSX</p>
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default App;
