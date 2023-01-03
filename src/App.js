2ef721948322dcabaa89a03908d4307e474e863c
Direwolf121 <jefflin1201@gmail.com>
2023-01-03 17:11:41 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,10 @@
 import React from 'react';
 import './App.css';
-
-const displayEmojiName = event => alert(event.target.id);
-const emojis = [
-  {
-    emoji: '😀',
-    name: "test grinning face"
-  },
-  {
-    emoji: '🎉',
-    name: "party popper"
-  },
-  {
-    emoji: '💃',
-    name: "woman dancing"
-  },
-  {
-    emoji: '😄',
-    name: "笑顔"
-  }
-];
+import EmojiTable from '.components/EmojiTable.js'
 
 function App() {
-  const greeting = "greeting";
-  const displayLove = true;
-
   return(
-    <div className="container">
-      <h1 id={greeting}>Hello, World</h1>
-      {displayLove && <p>我愛韓寶寶💕</p>}
-      <ul>
-        {
-          emojis.map(emoji => (
-            <li key={emoji.name}>
-              <button
-                onClick={displayEmojiName}
-              >
-                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
-              </button>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+    <EmojiTable />
   )
 }
 
---
+++ src/components/EmojiTable.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const displayEmojiName = event => alert(event.target.id);
+const greeting = "greeting";
+const displayLove = true;
+const emojis = [
+  {
+    emoji: '😀',
+    name: "test grinning face"
+  },
+  {
+    emoji: '🎉',
+    name: "party popper"
+  },
+  {
+    emoji: '💃',
+    name: "woman dancing"
+  },
+  {
+    emoji: '😄',
+    name: "笑顔"
+  }
+];
+
+
+const EmojiTable = () => (
+  <div className="EmojiTable">
+    <h1 id={greeting}>Hello, World</h1>
+      {displayLove && <p>我愛韓寶寶💕</p>}
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name}>
+              <button
+                onClick={displayEmojiName}
+              >
+                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+)
+
+export default EmojiTable;
